51074774cd7e034713f24871a1e59c844b79eca7
Miguel Suescun <debian789@gmail.com>
2015-12-25 23:42:51 -0500

    Guardar descripcion blog en html por medio de markdown

--- cliente/components/privado/blog/Form-blog.js
+++ cliente/components/privado/blog/Form-blog.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import marked from 'marked'
+import markedify from 'markedify'
 
 module.exports = class FormBlog extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state={
+      textBase:''
+    }
+  }
+  handleTextoBase (event) {
+    let textoIngresado = event.target.value
+    this.setState({textBase:marked(textoIngresado)})
+  }
+
+  convertirHtmlPrevio() {
+    return {__html:this.state.textBase}
+  }
   render () {
     return (
-      <form method='POST' action='/api/blog'>
+      <div>
+        <textarea onChange={this.handleTextoBase.bind(this)}></textarea>
+        <div dangerouslySetInnerHTML={this.convertirHtmlPrevio()}/>
+        <form method='POST' action='/api/blog' >
         <input type='text' name='titulo'/>
-        <textarea name='descripcion'></textarea>
+        <textarea name='descripcion' value={this.state.textBase}></textarea>
         <input type='submit' value='Guardar'/>
       </form>
+
+      </div>
     )
   }
 }
